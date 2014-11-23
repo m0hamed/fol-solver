3f3d097034828bb2fade53a6cde4ad94f36f734f
hossam <hossam693@gmail.com>
2014-11-24 00:53:53 +0200

    basic operations definitions and negations. Also, removed the negated boolean

--- fol.py
+++ fol.py
@@ -1,5 +1,5 @@
 import pdb
-
+from copy import deepcopy
 class Atom():
   pass
 
@@ -40,14 +40,49 @@ class Predicate(Nested):
   def negate(self):
     self.negated = not self.negated
 
+  def __str__(self):
+    if self.negated:
+      return "NOT[ " + super(Predicate,self).__str__() + " ]"
+    else:
+      return super(Predicate,self).__str__()
 
-class Conjunction():
+
+class And():
   def __init__(self, predicates=[]):
     self.children = predicates
 
-class Disjunction():
+  def negate(self):
+    temp = deepcopy(self.children)
+    for x in temp:
+      # pdb.set_trace()
+      x.negate()
+    return Or(temp)
+
+  def __str__(self):
+    s = " { "
+    for x in self.children:
+      s += x.__str__()
+      s += " } . { "
+    s = s[:-4]
+    return s
+
+class Or():
   def __init__(self, predicates=[]):
     self.children = predicates
+
+  def negate(self):
+    temp = deepcopy(self.children)
+    for x in temp:
+      x.negate()
+    return And(temp)
+
+  def __str__(self):
+    s = " { "
+    for x in self.children:
+      s += x.__str__()
+      s += " } | { "
+    s = s[:-4]
+    return s
 
 class Quantifier():
   def push_negation(self):
@@ -57,18 +92,61 @@ class Quantifier():
       return self
 
 class ForAll(Quantifier):
-  def __init__(self, variable, statement, negated=False):
+  def __init__(self, variable, statement):
     self.variable = variable
     self.statement = statement
-    self.negated = negated
 
   def negate(self):
-    self.negated = not self.negated
+    return ThereExists(self.variable,self.statement.negate())
+
+  def __str__(self):
+    s = "FOR_ALL(" + self.variable.__str__() + ") {" + self.statement.__str__() + "}"
+    return s
+
+class ThereExists(Quantifier):
+  def __init__(self, variable, statement):
+    self.variable = variable
+    self.statement = statement
+
+  def negate(self):
+    return ForAll(self.variable,self.statement.negate())
+
+  def __str__(self):
+    s = "THERE_EXISTS(" + self.variable.__str__() + ") {" + self.statement.__str__() + "}"
+    return s
 
 class Implication():
   def __init__(self, anticedent, consequent):
     self.anticedent = anticedent
     self.consequent = consequent
+
+  def get_not_or_form(self):
+    return Or([self.anticedent.negate, self.consequent])
+
+  def __str__(self):
+    s = self.anticedent.__str__() + " -> " + self.consequent.__str__()
+    return s
+
+  def negate(self):
+    # pdb.set_trace()
+    return And([self.anticedent,self.consequent.negate()])
+
+class Equivalence():
+  def __init__(self, statement1, statement2):
+    self.statement1 = statement1
+    self.statement2 = statement2
+
+  def get_implication_form(self):
+    return And([Implication(self.statement1, self.statement2),
+      Implication(self.statement2, self.statement1)])
+
+  def __str__(self):
+    s = self.statement1.__str__() + " <-> " + self.statement2.__str__()
+    return s
+
+  def negate(self):
+    return Or([Implication(self.statement1, self.statement2).negate(),
+      Implication(self.statement2, self.statement1).negate()])
 
 class Substitution:
   def __init__(self, variable, replacement):
@@ -161,3 +239,37 @@ if __name__ == "__main__":
   else:
     print("MGU: ", ", ".join(map(str, mgu)))
 
+  print("\n\n\n\nStart of conjunctive normal form")
+
+  e1 = And([Predicate("P", Function("f", Variable("x"))), Predicate("P",
+        Function("g", Variable("x")))])
+  e2 = Or([Predicate("P", Function("f", Variable("x"))), Predicate("P",
+        Function("g", Variable("x")))])
+  e3 = Implication(e1, e2)
+  e4 = Equivalence(e1,e2)
+  e5 = ThereExists(Variable("x"), And([Predicate("P", Function("f", Variable("x"))),
+        Predicate("P", Function("g", Variable("x")))]))
+  e6 = ForAll(Variable("x"), And([Predicate("P", Function("f", Variable("x"))),
+        Predicate("P", Function("g", Variable("x")))]))
+  print("Orignal Expression:")
+  print("Expression 1: ", e1)
+  print("Expression 2: ", e2)
+  print("Expression 3: ", e3)
+  print("Expression 4: ", e4)
+  print("Expression 5: ", e5)
+  print("Expression 6: ", e6)
+
+  print("\n\nNegated Expressions")
+
+  e7 = e1.negate()
+  e8 = e2.negate()
+  e9 = e3.negate()
+  e10 = e4.negate()
+  e11 = e5.negate()
+  e12 = e6.negate()
+  print("Expression 7: ", e7)
+  print("Expression 8: ", e8)
+  print("Expression 9: ", e9)
+  print("Expression 10: ", e10)
+  print("Expression 11: ", e11)
+  print("Expression 12: ", e12)
