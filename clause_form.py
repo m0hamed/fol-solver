22a706b6008cd296ab27d14f609e29b7334ce8eb
Mohamed Ashraf <mohamed.ashraf.213@gmail.com>
2014-11-29 16:41:13 +0200

    Fixed indentation in one example to make it more clear

--- clause_form.py
+++ clause_form.py
@@ -145,11 +145,21 @@ def cnf(statement):
   print(statement)
 
 if __name__ == "__main__":
-  p1 = Predicate('P', Variable('x'))
-  p2 = Predicate('Q', Variable('x'))
-  p3 = Predicate('Q', Variable('y'))
-  f1 = Predicate('R', Variable('y'), Variable('x'))
-  expression = ThereExists('x', Equivalence(p1, And(p2, ThereExists('y',And(p3,f1) )) ) )
+  expression = \
+    ThereExists('x',
+      Equivalence(
+        Predicate('P', Variable('x')),
+        And(
+          Predicate('Q', Variable('x')),
+          ThereExists('y',
+            And(
+              Predicate('Q', Variable('y')),
+              Predicate('R', Variable('y'), Variable('x'))
+            )
+          )
+        )
+      )
+    )
   cnf(expression)
 
   e = Or(And(Variable('x'), Or(Variable('w'), Variable('v'))),
